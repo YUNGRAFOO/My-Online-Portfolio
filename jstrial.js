4fe1879107f6ae4826492994b2f5b031e33d3ba4
YUNGRAFOO <rafidmahmud2@gmail.com>
2025-03-19 14:49:15 -0400

    Add files via upload

---
+++ jstrial.js
@@ -0,0 +1,10 @@
+function correct(){
+    <audio play>
+        <a href="https://www.myinstants.com/en/instant/game-show-correct-33552/?utm_source=copy&utm_medium=share" download="https://www.myinstants.com/en/instant/game-show-correct-33552/?utm_source=copy&utm_medium=share">MP3</a>
+            </audio>
+}
+function wrong(){
+    <audio play>
+        <a href="https://www.myinstants.com/en/instant/wrong-answer-gameshow/?utm_source=copy&utm_medium=share" download="https://www.myinstants.com/en/instant/wrong-answer-gameshow/?utm_source=copy&utm_medium=share">MP3</a>
+            </audio>
+}
